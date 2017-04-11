6718309c14c1f0004409163c8b30d1f9363b3abb
liutak999 <liutak999@gmail.com>
2017-04-12 02:09:24 +0800

    table.h
    first trial

---
+++ table.h
@@ -0,0 +1,16 @@
+#ifndef TABLE   /* Include guard */
+#define TABLE
+
+struct IP_PORT;
+struct Entry ;
+struct Entry* hashArray[SIZE];
+struct Entry* dummyEntry;
+struct Entry* entry;
+
+int hashCode(int key);
+struct Entry *search(int key);
+void insert(int key, struct IP_PORT *wan, struct IP_PORT *lan);
+struct Entry* delete(struct Entry* entry);
+void display();
+
+#endif
